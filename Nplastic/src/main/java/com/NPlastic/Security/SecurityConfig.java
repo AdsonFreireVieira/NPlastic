fefb70f1e83e1e72ac672e5557ca9e1a6d65f5dc
Adson <adsonfr.vieira@hotmail.com>
2025-05-16 23:51:26 -0300

    add:Security

---
+++ Nplastic/src/main/java/com/NPlastic/Security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.NPlastic.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class SecurityConfig {
+}
