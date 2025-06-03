85a1e961887767526d52b87852b87e6656996f70
Adson <adsonfr.vieira@hotmail.com>
2025-06-02 21:39:44 -0300

    init

---
+++ NPlastic/src/main/java/com/NPlastic/NPlasticApplication.java
@@ -0,0 +1,13 @@
+package com.NPlastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NPlasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NPlasticApplication.class, args);
+	}
+
+}
